396dda28f7de0a71b6307de5c1bade637a882c8b
khj-95 <kim37171@cuk.edu>
2021-09-15 20:28:41 +0900

    [김현진] local conflict

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,11 +2,11 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "[GITHUB]local GIT Test용 클레스 입니다.";
-	private static final String DEV = "dev브랜치 테스트 시작[local]";
-	private static final String DEV_FIN = "[GITHUB]dev 브랜치 테스트 종료";
+	private static final String DESC = "local GIT Test용 클레스 입니다.[local]";
+	private static final String DEV = "dev브랜치 테스트 시작";
+	private static final String DEV_FIN = "dev 브랜치 테스트 종료[local]";
 	private static final String CONFLICT = "Conflict 테스트";
-	private static final String MERGETOOL = "merge tool 사용";
+	private static final String MERGETOOL = "merge tool 사용[local]";
 	
 
 }
