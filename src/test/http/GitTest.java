912379c0314a551d5bda03d14cf3f26b796d4919
khj-95 <kim37171@cuk.edu>
2021-09-15 20:31:06 +0900

    [김현진] local Conflict

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -3,10 +3,15 @@ package test.http;
 public class GitTest {
 	
 	private static final String DESC = "local GIT Test용 클레스 입니다.[local]";
+<<<<<<< HEAD
 	private static final String DEV = "dev브랜치 테스트 시작";
 	private static final String DEV_FIN = "dev 브랜치 테스트 종료[local]";
 	private static final String CONFLICT = "Conflict 테스트";
 	private static final String MERGETOOL = "merge tool 사용[local]";
 	
+=======
+	private static final String DEV = "dev브랜치 테스트 시작[local]";
+	private static final String DEV_FIN = "dev 브랜치 테스트 종료[local]";
+>>>>>>> refs/heads/dev
 
 }
